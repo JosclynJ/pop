674084440a191cd8cd1cdbfb1ee41dd26c0b7e2a
JosclynJ <josclynjj@gmail.com>
2025-06-26 20:53:06 +0800

    Nambahin dashboard

--- pop/views.py
+++ pop/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User 
+from berita.models import *
 
 def dashboard(request):
     template_name = "dashboard/index.html"
-
+    jumlah_user = User.objects.count()
+    jumlah_berita = Berita.objects.count()
     context = {
-
+        'jumlah_user':jumlah_user,
+        'jumlah_berita':jumlah_berita
     }
     return render(request, template_name, context)
